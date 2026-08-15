c3fa3c96805926e931cdba5fe653f2cc29e8d5ef
Bleu Finley <mrbifinley@gmail.com>
2026-08-15 03:06:42 -0700

    Demo Test

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float movementSpeed = 3.0f;
+    private float sprintSpeed = 4.5f;
+    
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
